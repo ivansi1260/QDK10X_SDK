164882524d5cdbd26140f8359e87d4c46213f417
ivan.si <ivan.si@ikotek.com>
2026-06-12 11:25:24 +0800

    initial commit

---
+++ env.sh
@@ -0,0 +1,3 @@
+apt update
+apt install gcc g++ binutils bzip2 flex python3 python3-setuptools perl make findutils grep diffutils unzip gawk subversion libz-dev libc-dev rsync libncurses-dev
+cp config.ok .config
